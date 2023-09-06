c5a8b14a71aaee6e99afb056c1ea47a1f4956b75
JinHo Shin <dpdlalslr@gmail.com>
2023-09-06 19:42:23 +0900

    feat(FE): google 로그인 BUS-151

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,18 +3,31 @@
 export default function Home(): any {
   const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_URL;
   const KAKAO_REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
-  console.log(KAKAO_REST_API_KEY);
+  const GOOGLE_REST_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_REST_API_URL;
+  const GOOGLE_REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
   const kakaoLogin = () => {
     window.open(
-      `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`
+      `https://kauth.kakao.com/oauth/authorize` +
+        `?client_id=${KAKAO_REST_API_KEY}` +
+        `&redirect_uri=${KAKAO_REDIRECT_URI}` +
+        `&response_type=code`
     );
   };
+  const googleLogin = () => {
+    window.open(
+      `https://accounts.google.com/o/oauth2/v2/auth` +
+        `?client_id=${GOOGLE_REST_API_KEY}` +
+        `&redirect_uri=${GOOGLE_REDIRECT_URI}` +
+        `&response_type=code` +
+        `&scope=email`
+    );
+  };
+
   return (
     <>
       <button
         style={{
           position: 'relative',
-          top: '50%',
           left: '50%',
           width: '200px',
           height: '40px',
@@ -26,11 +39,31 @@ export default function Home(): any {
           fontWeight: 'bold',
           cursor: 'pointer',
           marginLeft: '-100px',
-          marginTop: '-20px',
+          marginTop: '20px',
         }}
         onClick={kakaoLogin}
       >
         카카오 로그인
+      </button>
+      <button
+        style={{
+          position: 'relative',
+          left: '50%',
+          width: '200px',
+          height: '40px',
+          background: '#09f',
+          color: '#fff',
+          border: 'none',
+          borderRadius: '6px',
+          fontSize: '15px',
+          fontWeight: 'bold',
+          cursor: 'pointer',
+          marginLeft: '-100px',
+          marginTop: '20px',
+        }}
+        onClick={googleLogin}
+      >
+        구글 로그인
       </button>
     </>
   );
