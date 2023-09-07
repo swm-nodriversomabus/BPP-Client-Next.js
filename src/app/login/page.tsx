01a8a2c29f8b58da2b62f97d67a13c3586a48f6d
JinHo Shin <dpdlalslr@gmail.com>
2023-09-07 14:44:32 +0900

    feat(FE): 네이버 로그인 추가 BUS-151

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 export default function Home(): any {
-  const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_URL;
+  const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
   const KAKAO_REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
-  const GOOGLE_REST_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_REST_API_URL;
+  const GOOGLE_REST_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_REST_API_KEY;
   const GOOGLE_REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
+  const NAVER_REST_API_KEY = process.env.NEXT_PUBLIC_NAVER_REST_API_KEY;
+  const NAVER_REDIRECT_URI = process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI;
+  const NAVER_STATE_STRING = process.env.NEXT_PUBLIC_NAVER_STATE_STRING;
   const kakaoLogin = () => {
     window.open(
       `https://kauth.kakao.com/oauth/authorize` +
@@ -20,6 +23,15 @@ export default function Home(): any {
         `&redirect_uri=${GOOGLE_REDIRECT_URI}` +
         `&response_type=code` +
         `&scope=email`
+    );
+  };
+  const naverLogin = () => {
+    window.open(
+      `https://nid.naver.com/oauth2.0/authorize` +
+        `?client_id=${NAVER_REST_API_KEY}` +
+        `&redirect_uri=${NAVER_REDIRECT_URI}` +
+        `&response_type=code` +
+        `&state=${encodeURI(NAVER_STATE_STRING!)}`
     );
   };
 
@@ -64,6 +76,26 @@ export default function Home(): any {
         onClick={googleLogin}
       >
         구글 로그인
+      </button>
+      <button
+        style={{
+          position: 'relative',
+          left: '50%',
+          width: '200px',
+          height: '40px',
+          background: '#09f',
+          color: '#fff',
+          border: 'none',
+          borderRadius: '6px',
+          fontSize: '15px',
+          fontWeight: 'bold',
+          cursor: 'pointer',
+          marginLeft: '-100px',
+          marginTop: '20px',
+        }}
+        onClick={naverLogin}
+      >
+        네이버 로그인
       </button>
     </>
   );
