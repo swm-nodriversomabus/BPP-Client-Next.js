73dfcdfb1c4cc075eebd86a93629d34cf62b437a
JinHo Shin <dpdlalslr@gmail.com>
2023-10-18 20:22:31 +0900

    feat(FE): 알림페이지에 승인됨 거절됨 추가 BUS-191

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -6,8 +6,37 @@ import ContentBox from '../../component/contentBox';
 import { useState } from 'react';
 import api, { mapping } from '@/utils/api';
 import Image from 'next/image';
-import noti_new from 'public/noti_new.svg';
 import { useRouter } from 'next/navigation';
+import noti_new from 'public/noti_new.svg';
+import noti_approved from 'public/noti_approved.svg';
+import noti_reject from 'public/noti_reject.svg';
+
+// [
+//   {
+//     notificationId: 1,
+//     type: '1',
+//     content: 'string',
+//     createdAt: [2023, 10, 18, 8, 53, 45],
+//     updatedAt: [2023, 10, 18, 8, 53, 45],
+//     isActive: true,
+//   },
+//   {
+//     notificationId: 1,
+//     type: '2',
+//     content: 'string',
+//     createdAt: [2023, 10, 18, 8, 53, 45],
+//     updatedAt: [2023, 10, 18, 8, 53, 45],
+//     isActive: true,
+//   },
+//   {
+//     notificationId: 1,
+//     type: '3',
+//     content: 'string',
+//     createdAt: [2023, 10, 18, 8, 53, 45],
+//     updatedAt: [2023, 10, 18, 8, 53, 45],
+//     isActive: true,
+//   },
+// ]
 
 export default function Home(): any {
   const router = useRouter();
@@ -19,13 +48,14 @@ export default function Home(): any {
       <ContentBox>
         {mapping(notification, (item: any, index: number) => {
           switch (item.type) {
-            default:
+            case '1':
               return (
                 <div
                   className="NotificationItem"
                   onClick={() => {
-                    // router.push(`../match/room/${1}`);
+                    router.push(`../match/room/${1}`);
                   }}
+                  key={index}
                 >
                   <Image src={noti_new} alt="icon" />
                   <div>{'메이트 신청 알림'}</div>
@@ -34,7 +64,40 @@ export default function Home(): any {
                   <div>{`${item.createdAt[0]}.${item.createdAt[1]}.${item.createdAt[2]}`}</div>
                 </div>
               );
-              break;
+            case '2':
+              return (
+                <div
+                  className="NotificationItem"
+                  onClick={() => {
+                    router.push(`../match/room/${1}`);
+                  }}
+                  key={index}
+                >
+                  <Image src={noti_approved} alt="icon" />
+                  <div>{'메이트 성사 알림'}</div>
+                  <div>{'함께 할 메이트가 생겼어요!'}</div>
+                  <div>{item.content}</div>
+                  <div>{`${item.createdAt[0]}.${item.createdAt[1]}.${item.createdAt[2]}`}</div>
+                </div>
+              );
+            case '3':
+              return (
+                <div
+                  className="NotificationItem"
+                  onClick={() => {
+                    // router.push(`../match/room/${1}`);
+                  }}
+                  key={index}
+                >
+                  <Image src={noti_reject} alt="icon" />
+                  <div>{'메이트 거절 알림'}</div>
+                  <div>{'메이트 신청이 거절되었어요.'}</div>
+                  <div>{item.content}</div>
+                  <div>{`${item.createdAt[0]}.${item.createdAt[1]}.${item.createdAt[2]}`}</div>
+                </div>
+              );
+            default:
+              return <></>;
           }
         })}
       </ContentBox>
