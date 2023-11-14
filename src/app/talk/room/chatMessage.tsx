578aaff8269c8ff895b233e3e21f0a9f5f430207
JinHo Shin <dpdlalslr@gmail.com>
2023-11-15 03:05:28 +0900

    feat(FE): 시간 반영 BUS-191

--- src/app/talk/room/chatMessage.tsx
+++ src/app/talk/room/chatMessage.tsx
@@ -1,18 +1,26 @@
 import { UIEventHandler } from 'react';
 import Image from 'next/image';
-import profile0 from 'public/profile0.svg';
-import profile9 from 'public/profile9.svg';
+import emptyProfile from 'public/empty_profile.png';
 
 interface propsType {
   received: string | undefined;
   children: JSX.Element;
+  timestamp: number[];
 }
 
-export default function ChatMessage({ received, children }: propsType) {
+export default function ChatMessage({
+  received,
+  children,
+  timestamp,
+}: propsType) {
   const texts = children + '';
   if (received)
-    return <MessageReceived received={received}>{texts}</MessageReceived>;
-  else return <MessageSent>{texts}</MessageSent>;
+    return (
+      <MessageReceived received={received} timestamp={timestamp}>
+        {texts}
+      </MessageReceived>
+    );
+  else return <MessageSent timestamp={timestamp}>{texts}</MessageSent>;
 }
 
 const MessageReceived: any = (Props: propsType) => {
@@ -20,29 +28,42 @@ const MessageReceived: any = (Props: propsType) => {
     <>
       <div className="message received">
         <div>
-          <Image
-            width={32}
-            src={localStorage.getItem('tid') == '1' ? profile0 : profile9}
-            alt="image"
-          />
+          <Image width={32} src={emptyProfile} alt="image" />
         </div>
         <div>{Props.received}</div>
         <div>
           {Props.children}
-          <div>오전 09:41</div>
+          <div>{`${Props.timestamp[3] < 12 ? '오전' : '오후'} ${(
+            ((Props.timestamp[3] + 11) % 12) +
+            101
+          )
+            .toString()
+            .substring(1)}:${(Props.timestamp[4] + 100)
+            .toString()
+            .substring(1)}`}</div>
         </div>
       </div>
     </>
   );
 };
 
-const MessageSent: any = (Props: { children: JSX.Element }) => {
+const MessageSent: any = (Props: {
+  children: JSX.Element;
+  timestamp: number[];
+}) => {
   return (
     <>
       <div className="message sent">
         <div>
           {Props.children}
-          <div>오전 09:41</div>
+          <div>{`${Props.timestamp[3] < 12 ? '오전' : '오후'} ${(
+            ((Props.timestamp[3] + 11) % 12) +
+            101
+          )
+            .toString()
+            .substring(1)}:${(Props.timestamp[4] + 100)
+            .toString()
+            .substring(1)}`}</div>
         </div>
       </div>
     </>
