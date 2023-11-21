43d5ec92f27c94f06fb04cee22d2e0f9f1aa921a
JinHo Shin <dpdlalslr@gmail.com>
2023-11-22 03:56:11 +0900

    feat(FE): 프로필 이미지 업로드 api 적용 BUS-191

--- src/app/mypage/edit/page.tsx
+++ src/app/mypage/edit/page.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/component/navigationBar';
 import ContentBox from '@/component/contentBox';
 import emptyProfile from 'public/empty_profile.png';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { RefObject, useEffect, useRef, useState } from 'react';
 import api from '@/utils/api';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
@@ -13,9 +13,14 @@ import { useRouter } from 'next/navigation';
 export default function Home(): any {
   const router = useRouter();
   const [myInfo, setMyInfo] = useState<JSON | null>(null);
+  const [myImage, setMyImage] = useState<JSON | null>(null);
   const [username, setUsername] = useState('');
   const [stateMessage, setStateMessage] = useState('');
+
+  const imageUpload: RefObject<HTMLInputElement> =
+    useRef<HTMLInputElement>(null);
   api('user', 'get', {}, [myInfo, setMyInfo]);
+  api('user/image', 'get', {}, [myImage, setMyImage]);
   useEffect(() => {
     if (!myInfo || !('username' in myInfo) || !('stateMessage' in myInfo))
       return;
@@ -74,7 +79,15 @@ export default function Home(): any {
       </Navbar>
       <ContentBox>
         <div className="MyProfile">
-          <Image src={emptyProfile} width={72} alt="image" />
+          <Image
+            src={emptyProfile}
+            width={72}
+            height={72}
+            alt="image"
+            onClick={() => {
+              imageUpload.current?.click();
+            }}
+          />
           <input
             onChange={(e: any) => {
               setUsername(
@@ -96,6 +109,32 @@ export default function Home(): any {
           />
         </div>
       </ContentBox>
+      <input
+        type="file"
+        ref={imageUpload}
+        style={{ display: 'none' }}
+        accept="image/*"
+        onChange={(e) => {
+          if (e.currentTarget.files && e.currentTarget.files.length) {
+            if (e.currentTarget.files[0]) {
+              const BASE_URL = process.env.NEXT_BASE_URL;
+              fetch(BASE_URL + 'user/image', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': e.currentTarget.files[0].type,
+                },
+                credentials: 'include',
+                body: e.currentTarget.files[0],
+              }).then((res: Response) => {
+                if (res.status == 200) {
+                  // 정상 응답 확인된 경우, 응답 받은 JSON 전달}
+                  console.log(200);
+                }
+              });
+            }
+          }
+        }}
+      />
     </>
   );
 }
