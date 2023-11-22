fc940d91875db92ff2d8ec626a30840a34b2773a
JinHo Shin <dpdlalslr@gmail.com>
2023-11-22 18:54:34 +0900

    feat(FE): 매칭 페이지에서 사용자 프로필사진 BUS-191

--- src/component/matchPeople.tsx
+++ src/component/matchPeople.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
 import emptyProfile from 'public/empty_profile.png';
+import { useState } from 'react';
 
 interface props {
   children: string | undefined;
@@ -33,6 +35,11 @@ const MatchPerson: any = ({
   setCandidate: (arg0: any) => void;
   userId: string;
 }) => {
+  const [isImage, setIsImage] = useState(true);
+  const BASE_URL: string = process.env.NEXT_BASE_URL
+    ? process.env.NEXT_BASE_URL
+    : '';
+  const CLIENT: string = process.env.NEXT_CLIENT ? process.env.NEXT_CLIENT : '';
   return (
     <div
       className="MatchPerson"
@@ -48,7 +55,19 @@ const MatchPerson: any = ({
       }}
     >
       <div>
-        <Image width={48} height={48} src={emptyProfile} alt="profile" />
+        {isImage ? (
+          <img
+            src={userId ? BASE_URL + 'user/image/' + userId : ''}
+            onError={(e) => {
+              setIsImage(false);
+            }}
+            width={48}
+            height={48}
+            alt="image"
+          />
+        ) : (
+          <Image src={emptyProfile} width={48} height={48} alt="image" />
+        )}
       </div>
       <div>{username}</div>
       {/* <div>20대 초반</div> */}
